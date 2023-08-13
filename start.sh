57a1c48402f488f30f9c9c93a578483d25c57128
KIM <konata4720@gmail.com>
2023-08-13 15:21:37 +0000

    first commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+mkdir ./main
+mkdir ./data
+mkdir ./percona/mysql
+mkdir ./redis/data
+
+chmod 777 ./percona/mysql
+chmod 777 ./percona/start-container
+chmod 777 ./php/start-container
+chmod 777 ./nginx/start-container
+chmod 777 ./redis/data
+chmod 777 ./redis/start-container
+
+
